cbea823d16bc080e3bf6d115071b50fb55ab3ae1
Adel <adeloabdallah1995@gmail.com>
2019-12-01 18:12:35 -0500

    Day 1 part 2 complete
    formatting

--- day1/day1.py
+++ day1/day1.py
@@ -2,9 +2,33 @@ file = open('./day1/input.txt', 'r')
 lines = file.read().splitlines()
 file.close()
 
+# Part 1
+
 total = 0
 
 for num in lines:
     total += (int(num) // 3) - 2
 
 print(total)
+
+# Part 2
+
+recursiveTotal = 0
+
+
+def addFuelRecursively(num):
+    global recursiveTotal
+    amountToAdd = (int(num) // 3) - 2
+
+    if (amountToAdd <= 0):
+        return
+
+    recursiveTotal += amountToAdd
+
+    addFuelRecursively(amountToAdd)
+
+
+for num in lines:
+    addFuelRecursively(num)
+
+print(recursiveTotal)
