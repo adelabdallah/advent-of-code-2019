f79867143b9721bc7f87cbaedc96ac82fac28441
Adel <adeloabdallah1995@gmail.com>
2019-12-08 23:57:29 -0500

    Part 1 complete
    Readme amend

--- day8/day8.py
+++ day8/day8.py
@@ -1,42 +1,47 @@
-def divideByWidth(rawInput, width):
-    i = width
-    j = 0
+def divideByDimension(rawInput, dimension):
     divided = []
+    i = dimension
+    j = 0
     while i <= len(rawInput):
         divided.append(rawInput[j:i])
         j = i
-        i += width
+        i += dimension
 
     return divided
 
 
-def divideByHeight(dividedInput, height):
-    layers = []
-    i = height
-    j = 0
-    while i <= len(dividedInput):
-        layers.append(dividedInput[j:i])
-        j = i
-        i += height
-
-    return layers
+def getLayers(rawInput, width, height):
+    return divideByDimension(divideByDimension(rawInput, width), height)
 
 
-def countZeros(strInput):
-    return len(strInput) - len(strInput.rstrip('0'))
+def countDigit(strInput, digit):
+    return len(strInput) - len(strInput.replace(digit, ""))
 
 
 def findLeastZeros(layers):
     layerCounts = {}
-    count = 0
     for i in range(len(layers)):
+        layerCounts[i] = 0
         for j in range(len(layers[i])):
-            layerCounts[i] += countZeros(layers[j])
-        count = layerCounts[i] if layerCounts[i] > count else count
+            layerCounts[i] += countDigit(layers[i][j], "0")
 
-    return count
+    return min(layerCounts, key=layerCounts.get)
 
 
-print(divideByWidth("123456789012", 3))
-print(divideByHeight(divideByWidth("123456789012", 3), 2))
-print(findLeastZeros(divideByHeight(divideByWidth("123456789012", 3), 2)))
+def multiplyDigits(rawInput, width, height, digit1, digit2):
+    countDigit1 = 0
+    countDigit2 = 0
+
+    layer = getLayers(rawInput, width, height)[
+        findLeastZeros(getLayers(rawInput, width, height))]
+
+    for i in range(len(layer)):
+        countDigit1 += countDigit(layer[i], "1")
+        countDigit2 += countDigit(layer[i], "2")
+
+    return countDigit1 * countDigit2
+
+
+with open('./day8/input.txt') as _file:
+    for line in _file.readlines():
+        print(multiplyDigits(line, 25, 6, "1", "2"))
